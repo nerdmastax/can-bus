f0760fa38db5af09e36ef54958ce687e8308d56f
blixtz <blixtz@gmail.com>
2013-11-13 07:10:31 +0000

    original upload

---
+++ samples/routine83.sh
@@ -0,0 +1,53 @@
+./setconfig
+./checkmode
+./disablemask
+./setspeed 196 255 135
+./setnormal
+./checkmode
+./checkspeed
+./monitor
+
+./setconfig
+./checkmode
+./disablemask
+./setspeed 197 250 135
+./setnormal
+./checkmode
+./checkspeed
+./monitor
+
+./setconfig
+./checkmode
+./disablemask
+./setspeed 199 248 132
+./setnormal
+./checkmode
+./checkspeed
+./monitor
+
+./setconfig
+./checkmode
+./disablemask
+./setspeed 201 232 131
+./setnormal
+./checkmode
+./checkspeed
+./monitor
+
+./setconfig
+./checkmode
+./disablemask
+./setspeed 139 224 130
+./setnormal
+./checkmode
+./checkspeed
+./monitor
+
+./setconfig
+./checkmode
+./disablemask
+./setspeed 142 208 130
+./setnormal
+./checkmode
+./checkspeed
+./monitor
